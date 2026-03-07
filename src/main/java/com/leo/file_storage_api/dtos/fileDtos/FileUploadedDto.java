29e19cac2b264adf484eb7c1b846d5a4079d43d1
LeoGamerMan <mirzajanzadeh.leo@gmail.com>
2026-03-07 10:17:35 +0100

    FileUploadedDto, changed username to userID

--- src/main/java/com/leo/file_storage_api/dtos/fileDtos/FileUploadedDto.java
+++ src/main/java/com/leo/file_storage_api/dtos/fileDtos/FileUploadedDto.java
@@ -2,11 +2,13 @@ package com.leo.file_storage_api.dtos.fileDtos;
 
 import com.leo.file_storage_api.models.file.File;
 
+import java.util.UUID;
+
 public record FileUploadedDto(
-    String username,
+    UUID userID,
     int fileSize
 ) {
   public static FileUploadedDto from(File file) {
-    return new FileUploadedDto(file.getMap().getUser().getName(), file.getContent().length());
+    return new FileUploadedDto(file.getMap().getUser().getUserID(), file.getContent().length());
   }
 }
